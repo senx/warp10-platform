52beef42041b48c53732eef688e7f9a958d61404
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2016-09-24 01:00:31 +0200

    Fixed handling of store.skip.write

--- warp10/src/main/java/io/warp10/continuum/store/Store.java
+++ warp10/src/main/java/io/warp10/continuum/store/Store.java
@@ -189,7 +189,7 @@ public class Store extends Thread {
     this.keystore = keystore;
     this.properties = properties;
     
-    if ("true".equals(properties.containsKey("store.skip.write"))) {
+    if ("true".equals(properties.getProperty("store.skip.write"))) {
       this.SKIP_WRITE = true;
     } else {
       this.SKIP_WRITE = false;
