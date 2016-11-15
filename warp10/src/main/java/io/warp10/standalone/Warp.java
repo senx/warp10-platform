56658803ef52c5ebef89fb8dd6e1d4013aecc2b6
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2016-11-15 01:31:33 +0100

    Cleaning

--- warp10/src/main/java/io/warp10/standalone/Warp.java
+++ warp10/src/main/java/io/warp10/standalone/Warp.java
@@ -28,7 +28,6 @@ import io.warp10.continuum.egress.EgressFetchHandler;
 import io.warp10.continuum.egress.EgressFindHandler;
 import io.warp10.continuum.egress.EgressMobiusHandler;
 import io.warp10.continuum.ingress.DatalogForwarder;
-import io.warp10.continuum.sensision.SensisionConstants;
 import io.warp10.continuum.store.Constants;
 import io.warp10.continuum.store.StoreClient;
 import io.warp10.crypto.CryptoUtils;
@@ -38,14 +37,12 @@ import io.warp10.crypto.UnsecureKeyStore;
 import io.warp10.quasar.filter.QuasarTokenFilter;
 import io.warp10.script.ScriptRunner;
 import io.warp10.script.WarpScriptLib;
-import io.warp10.sensision.Sensision;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Properties;
 
-import org.apache.hadoop.util.ShutdownHookManager;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
@@ -207,13 +204,6 @@ public class Warp extends WarpDist implements Runnable {
     // Register shutdown hook to close the DB.
     Runtime.getRuntime().addShutdownHook(new Thread(new Warp()));
     
-    //
-    // Make sure ShutdownHookManager is initialized, otherwise it will try to
-    // register a shutdown hook during the shutdown hook we just registered...
-    //
-    
-    ShutdownHookManager.get();      
-
     //
     // Initialize the backup manager
     //
