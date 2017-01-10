fc33f519df052b8d19ebab08891834ff19f2b6ef
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2017-01-10 07:58:52 +0100

    Fixed missing mapper.parsedouble

--- warp10/src/main/java/io/warp10/script/WarpScriptLib.java
+++ warp10/src/main/java/io/warp10/script/WarpScriptLib.java
@@ -859,7 +859,7 @@ public class WarpScriptLib {
     functions.put("mapper.log", new MapperLog.Builder("mapper.log"));
     functions.put("mapper.min.x", new MapperMinX.Builder("mapper.min.x"));          
     functions.put("mapper.max.x", new MapperMaxX.Builder("mapper.max.x"));          
-
+    functions.put("mapper.parsedouble", new MapperParseDouble.Builder("mapper.parsedouble"));
     
     functions.put("mapper.tick", new MapperTick.Builder("mapper.tick"));
     functions.put("mapper.year", new MapperYear.Builder("mapper.year"));
