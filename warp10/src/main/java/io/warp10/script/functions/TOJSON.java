dfc5184508c9129e754299e842d379ff394c33de
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2017-03-01 10:53:56 +0100

    Modified the controls done on structure prior to their serialization. We avoid multiple references to a given list/map

--- warp10/src/main/java/io/warp10/script/functions/TOJSON.java
+++ warp10/src/main/java/io/warp10/script/functions/TOJSON.java
@@ -16,11 +16,15 @@
 
 package io.warp10.script.functions;
 
+import groovy.lang.Reference;
 import io.warp10.script.NamedWarpScriptFunction;
 import io.warp10.script.WarpScriptStackFunction;
 import io.warp10.script.WarpScriptException;
 import io.warp10.script.WarpScriptStack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -50,31 +54,110 @@ public class TOJSON extends NamedWarpScriptFunction implements WarpScriptStackFu
     // expose internals
     //
     
-    if (o instanceof List) {
-      for (Object elt: (List) o) {
-        if (!(elt instanceof Number) && !(elt instanceof String) && !(elt instanceof Boolean)) {
-          throw new WarpScriptException(getName() + " can only handle numeric, boolean and string types.");
-        }
-      }
-    } else if (o instanceof Map) {
-      for (Entry<Object, Object> entry: ((Map<Object,Object>) o).entrySet()) {
-        Object elt = entry.getKey();
-        if (!(elt instanceof Number) && !(elt instanceof String) && !(elt instanceof Boolean)) {
-          throw new WarpScriptException(getName() + " can only handle numeric, boolean and string types.");
-        }
-        elt = entry.getValue();
-        if (!(elt instanceof Number) && !(elt instanceof String) && !(elt instanceof Boolean)) {
-          throw new WarpScriptException(getName() + " can only handle numeric, boolean and string types.");
-        }
-      }
-    } else {
-      throw new WarpScriptException(getName() + " can only handle simple lists/maps.");
+    if (!validate(o)) {
+      throw new WarpScriptException(getName() + " can only serialize structures containing numbers, strings, booleans, lists and maps which do not reference the same list/map multiple times.");
     }
-    
+
     String json = parser.serialize(o).toString();
     
     stack.push(json);
     
     return stack;
+  }
+  
+  /**
+   * Validate an object for serialization.
+   * We only allow Number/String/Booleans and Lists/Map containing those elements or List/Map
+   * 
+   * @return true if the object is validated, false otherwise
+   */
+  private static boolean validate(Object o) {
+    
+    // Allocate a list for elements, we need to ensure there are
+    // no loops in the object we try to serialize so we enforce
+    // a more stringent rule which forces lists/maps to not be
+    // referenced several times in the structure we attempt to
+    // serialize
+    
+    List<Object> elements = new ArrayList<Object>();
+    
+    int idx = 0;
+    
+    
+    if (o instanceof Number || o instanceof String || o instanceof Boolean) {
+      return true;
+    }
+    
+    elements.add(o);
+    
+    while(idx < elements.size()) {
+      Object obj = elements.get(idx++);
+      
+      if (obj instanceof Number || obj instanceof String || obj instanceof Boolean) {
+        continue;
+      }
+
+      if (obj instanceof List) {
+        for (Object elt: (List) obj) {
+          if (elt instanceof Number || elt instanceof String || elt instanceof Boolean) {
+            continue;
+          } else if (elt instanceof List || elt instanceof Map) {
+            // Check that the given list/map is not already in the structure
+            if (containsElement(elements, elt)) {
+              return false;
+            }
+            elements.add(elt);
+          } else {
+            return false;
+          }
+        }
+      } else if (obj instanceof Map) {
+        for (Entry<Object,Object> entry: ((Map<Object,Object>) obj).entrySet()) {
+          Object elt = entry.getKey();
+          if (elt instanceof Number || elt instanceof String || elt instanceof Boolean) {
+            // Ignore keys which are atomic like types
+          } else if (elt instanceof List || elt instanceof Map) {
+            // Check that the given list/map is not already in the structure
+            if (containsElement(elements, elt)) {
+              return false;
+            }
+            elements.add(elt);
+          } else {
+            return false;
+          }
+          elt = entry.getValue();
+          if (elt instanceof Number || elt instanceof String || elt instanceof Boolean) {
+            continue;
+          } else if (elt instanceof List || elt instanceof Map) {
+            // Check that the given list/map is not already in the structure
+            if (containsElement(elements, elt)) {
+              return false;
+            }
+            elements.add(elt);
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+    
+    return true;
+  }
+  
+  /**
+   * Check if an actual object reference is already in a list
+
+   * @param list
+   * @param element
+   * @return
+   */
+  private static boolean containsElement(List<Object> list, Object element) {
+    for (Object o: list) {
+      if (o == element) {
+        return true;
+      }
+    }
+    
+    return false;
   }
 }
