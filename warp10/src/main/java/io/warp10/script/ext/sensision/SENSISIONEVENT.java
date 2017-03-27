96145d3a7d917b61d955d7299182b21389e1c5ef
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2017-03-27 17:14:43 +0200

    Added support for timestamp in event creation

--- warp10/src/main/java/io/warp10/script/ext/sensision/SENSISIONEVENT.java
+++ warp10/src/main/java/io/warp10/script/ext/sensision/SENSISIONEVENT.java
@@ -29,12 +29,21 @@ public class SENSISIONEVENT extends NamedWarpScriptFunction implements WarpScrip
     
     List<Object> args = (List<Object>) top;
         
-    String cls = args.get(0).toString();
-    Map<String,String> labels = (Map<String,String>) args.get(1);
-    Object value = args.get(2);
     
-    Sensision.event(cls, labels, value);
-
+    if (3 == args.size()) { // class,labels,value
+      String cls = args.get(0).toString();
+      Map<String,String> labels = (Map<String,String>) args.get(1);
+      Object value = args.get(2);
+      Sensision.event(cls, labels, value);
+    } else if (4 == args.size()) { // ts,class,labels,value
+      long ts = ((Number) args.get(0)).longValue();
+      String cls = args.get(1).toString();
+      Map<String,String> labels = (Map<String,String>) args.get(2);
+      Object value = args.get(3);
+    } else {
+      throw new WarpScriptException(getName() + " unsupported number of arguments.");
+    }
+    
     return stack;
   }
   
