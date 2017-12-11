20266a096ff120f83b853f363a412dc3c75b15f7
Collignon-Ducret Rémi <contact@rcdinfo.fr>
2017-12-11 08:42:24 +0000

    Fixed LASTACTIVITY comments and enable condition

--- warp10/src/main/java/io/warp10/script/functions/LASTACTIVITY.java
+++ warp10/src/main/java/io/warp10/script/functions/LASTACTIVITY.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2016  Cityzen Data
+//   Copyright 2017  Cityzen Data
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -16,8 +16,6 @@
 
 package io.warp10.script.functions;
 
-import io.warp10.WarpConfig;
-import io.warp10.continuum.Configuration;
 import io.warp10.continuum.gts.GTSEncoder;
 import io.warp10.continuum.gts.GeoTimeSerie;
 import io.warp10.script.NamedWarpScriptFunction;
@@ -25,31 +23,19 @@ import io.warp10.script.WarpScriptStackFunction;
 import io.warp10.script.WarpScriptException;
 import io.warp10.script.WarpScriptStack;
 
-import java.util.Properties;
 
 /**
  * Extract the last activity timestamp of the GTS on top of the stack
  */
 public class LASTACTIVITY extends NamedWarpScriptFunction implements WarpScriptStackFunction {
 
-  private final boolean enabled;
-
   public LASTACTIVITY(String name) {
     super(name);
-
-    Properties props = WarpConfig.getProperties();
-
-    this.enabled = null != props && ("true".equals(props.getProperty(Configuration.INGRESS_ACTIVITY_UPDATE)) || "true".equals(props.getProperty(Configuration.INGRESS_ACTIVITY_META)));
   }
 
   @Override
   public Object apply(WarpScriptStack stack) throws WarpScriptException {
     Object o = stack.pop();
-
-    // is gts activity activated ?
-    if (!this.enabled) {
-      throw new WarpScriptException(getName() + " need an activated ingress.activity configuration");
-    }
 
     if (!(o instanceof GeoTimeSerie) && !(o instanceof GTSEncoder)) {
       throw new WarpScriptException(getName() + " expects a Geo Time Series or encoder instance on top of the stack.");
