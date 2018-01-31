562b4384ef1435ed24b11a5318c8c7065769f6e2
Xavier Marin <marin.xavier@gmail.com>
2018-01-31 09:02:35 +0100

    jenkins

--- warp10/src/main/java/io/warp10/Revision.java
+++ warp10/src/main/java/io/warp10/Revision.java
@@ -1,5 +1,5 @@
 package io.warp10;
 
 public class Revision {
-  public static final String REVISION = "1.2.13-rc1-14-gcf019ba";
+  public static final String REVISION = "1.2.13-rc1-15-ge321624";
 }
