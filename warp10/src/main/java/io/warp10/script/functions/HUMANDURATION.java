dd20bd64cda3a909d88a01633c625cf617577135
Fabien Tencé <fabien.tence@cityzendata.com>
2018-03-22 10:02:11 +0100

    Fix for large and negative values for the timestamp or duration.

--- warp10/src/main/java/io/warp10/script/functions/HUMANDURATION.java
+++ warp10/src/main/java/io/warp10/script/functions/HUMANDURATION.java
@@ -26,57 +26,61 @@ import io.warp10.script.WarpScriptStack;
  * Convert a duration in time units into one suitable for humans...
  */
 public class HUMANDURATION extends NamedWarpScriptFunction implements WarpScriptStackFunction {
-  
+
   public HUMANDURATION(String name) {
     super(name);
   }
-  
+
   @Override
   public Object apply(WarpScriptStack stack) throws WarpScriptException {
     Object top = stack.pop();
-    
+
     if (!(top instanceof Long)) {
       throw new WarpScriptException(getName() + " expects a number of time units (LONG) on top of the stack.");
     }
-    
+
     long duration = ((Number) top).longValue();
-        
+
     StringBuilder sb = new StringBuilder();
-    
+
     long days = duration / (Constants.TIME_UNITS_PER_S * 86400L);
-    
-    if (days > 0) {
-      sb.append(days);
+
+    if (duration < 0) {
+      sb.append("-");
+    }
+
+    if (Math.abs(days) > 0) {
+      sb.append(Math.abs(days));
       sb.append("d");
       duration = duration - days * (Constants.TIME_UNITS_PER_S * 86400L);
     }
-    
+
     long hours = duration / (Constants.TIME_UNITS_PER_S * 3600L);
-    
-    if (hours > 0 || sb.length() > 0) {
-      sb.append(hours);
+
+    if (Math.abs(hours) > 0 || sb.length() > 0) {
+      sb.append(Math.abs(hours));
       sb.append("h");
       duration = duration - hours * (Constants.TIME_UNITS_PER_S * 3600L);
     }
-    
+
     long minutes = duration / (Constants.TIME_UNITS_PER_S * 60L);
-    
-    if (minutes > 0 || sb.length() > 0) {
-      sb.append(minutes);
+
+    if (Math.abs(minutes) > 0 || sb.length() > 0) {
+      sb.append(Math.abs(minutes));
       sb.append("m");
-      duration = duration - minutes * (Constants.TIME_UNITS_PER_S * 60L);      
+      duration = duration - minutes * (Constants.TIME_UNITS_PER_S * 60L);
     }
-    
+
     long seconds = duration / Constants.TIME_UNITS_PER_S;
-    
-    sb.append(seconds);
+
+    sb.append(Math.abs(seconds));
     duration = duration - seconds * Constants.TIME_UNITS_PER_S;
     sb.append(".");
-    sb.append(Long.toString(Constants.TIME_UNITS_PER_S + duration).substring(1));
+    sb.append(Long.toString(Constants.TIME_UNITS_PER_S + Math.abs(duration)).substring(1)); // Add leading 1 and remove with substring so as to pad with zeros
     sb.append("s");
-    
+
     stack.push(sb.toString());
-    
+
     return stack;
   }
 }
