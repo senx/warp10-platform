80343833d002cb7c453e46e1f272be582d5e7405
PAP1 <pierre.papin@cityzendata.com>
2018-04-24 15:20:35 +0200

    MAN code reformat

--- warp10/src/main/java/io/warp10/script/functions/MAN.java
+++ warp10/src/main/java/io/warp10/script/functions/MAN.java
@@ -1,3 +1,19 @@
+//
+//   Copyright 2018  Cityzen Data
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
 package io.warp10.script.functions;
 
 import io.warp10.continuum.store.Constants;
@@ -6,12 +22,17 @@ import io.warp10.script.WarpScriptStackFunction;
 import io.warp10.script.WarpScriptException;
 import io.warp10.script.WarpScriptStack;
 import io.warp10.script.WarpScriptLib;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import io.warp10.crypto.OrderPreservingBase64;
 import com.google.common.base.Charsets;
 
 public class MAN extends NamedWarpScriptFunction implements WarpScriptStackFunction {
+
+  private static final String MAN_FORBIDDEN_CHARS = ".*[!%&\\(\\)\\*+/<=>\\[\\]^\\{\\|\\}~].*";
+  private static Pattern manForbiddenCharsPattern = Pattern.compile(MAN_FORBIDDEN_CHARS);
 
   public MAN(String name) {
     super(name);
@@ -19,7 +40,8 @@ public class MAN extends NamedWarpScriptFunction implements WarpScriptStackFunct
 
   @Override
   public Object apply(WarpScriptStack stack) throws WarpScriptException {
-    String functionname=null;
+
+    String functionname = null;
 
     //
     // if there is a string on the top of the stack, take is as a function name
@@ -32,22 +54,18 @@ public class MAN extends NamedWarpScriptFunction implements WarpScriptStackFunct
       if (o instanceof String) {
         functionname = o.toString();
         if (!WarpScriptLib.getFunctionNames().contains(functionname)) {
-          functionname="";
+          functionname = "";
         }
       }
     }
 
     if (null == functionname) {
       stack.push(Constants.WARP10_DOC_URL);
-    }
-    else if ( "" == functionname) {
+    } else if ("" == functionname) {
       stack.push("Unknown function name, please check " + Constants.WARP10_DOC_URL);
-    }
-    else {
-      String forbiddenindoc = ".*[!%&\\(\\)\\*+/<=>\\[\\]^\\{\\|\\}~].*";
-      Pattern pattern = Pattern.compile(forbiddenindoc);
+    } else {
       String docname = functionname;
-      Matcher m = pattern.matcher(functionname);
+      Matcher m = manForbiddenCharsPattern.matcher(functionname);
       if (m.matches()
               || "-".equals(functionname)
               || "pi".equals(functionname)
