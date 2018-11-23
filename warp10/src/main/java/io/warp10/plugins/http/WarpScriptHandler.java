a230b24bd260f0ed1350d35a22a84e90da7f5399
Fabien Tencé <fabien.tence@senx.io>
2018-11-23 17:50:03 +0100

    Make handler only handle REQUEST. Avoids double exec on error.
    (cherry picked from commit a7c0d06acf96959aa73961ee7b5a9838c7ce3a31)

--- warp10/src/main/java/io/warp10/plugins/http/WarpScriptHandler.java
+++ warp10/src/main/java/io/warp10/plugins/http/WarpScriptHandler.java
@@ -24,6 +24,7 @@ import org.eclipse.jetty.http.MimeTypes;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -50,6 +51,12 @@ public class WarpScriptHandler extends AbstractHandler {
 
   @Override
   public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Only handle REQUEST
+    if(DispatcherType.REQUEST != baseRequest.getDispatcherType()){
+      baseRequest.setHandled(true);
+      return;
+    }
+
     String prefix = this.plugin.getPrefix(target);
     Macro macro = this.plugin.getMacro(prefix);
 
