ff993ea91e5e0d5eae1cefe700da1433fb0f836f
Fabien Tencé <fabien.tence@senx.io>
2019-03-08 14:32:53 +0100

    Fix copyright and use private function intances.

--- warp10/src/main/java/io/warp10/script/functions/TIMED.java
+++ warp10/src/main/java/io/warp10/script/functions/TIMED.java
@@ -1,3 +1,19 @@
+//
+//   Copyright 2018  SenX S.A.S.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
 package io.warp10.script.functions;
 
 import io.warp10.script.NamedWarpScriptFunction;
@@ -8,6 +24,11 @@ import io.warp10.script.WarpScriptStack.Macro;
 import io.warp10.script.WarpScriptStackFunction;
 
 public class TIMED extends NamedWarpScriptFunction implements WarpScriptStackFunction {
+
+  // Define private instances of used functions to avoid potential redefinition by extensions
+  private static final CHRONOEND funcCHRONOEND = new CHRONOEND(WarpScriptLib.CHRONOEND);
+  private static final CHRONOSTART funcCHRONOSTART = new CHRONOSTART(WarpScriptLib.CHRONOSTART);
+  private static final TRY funcTRY = new TRY(WarpScriptLib.TRY);
 
   public TIMED(String name) {
     super(name);
@@ -30,16 +51,16 @@ public class TIMED extends NamedWarpScriptFunction implements WarpScriptStackFun
     // Make TIMED resilient to STOP, RETURN and any exception.
     Macro finallyMacro = new Macro();
     finallyMacro.add(alias);
-    finallyMacro.add(WarpScriptLib.getFunction(WarpScriptLib.CHRONOEND));
+    finallyMacro.add(funcCHRONOEND);
 
     // Build a new macro starting with CHRONOSTART, calling the given macro and ending with CHRONOEND in the finally block of a TRY.
     Macro timedMacro = new Macro();
     timedMacro.add(alias);
-    timedMacro.add(WarpScriptLib.getFunction(WarpScriptLib.CHRONOSTART));
+    timedMacro.add(funcCHRONOSTART);
     timedMacro.add(macro);        // Try
     timedMacro.add(new Macro());  // Catch
     timedMacro.add(finallyMacro); // Finally
-    timedMacro.add(WarpScriptLib.getFunction(WarpScriptLib.TRY));
+    timedMacro.add(funcTRY);
 
     stack.push(timedMacro);
 
