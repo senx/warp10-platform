c95d57b3d5bf2b9c74998d374732551a596eb0d8
Fabien Tencé <fabien.tence@senx.io>
2019-04-03 13:54:18 +0200

    SUBLIST to take a proper range

--- warp10/src/main/java/io/warp10/script/functions/SUBLIST.java
+++ warp10/src/main/java/io/warp10/script/functions/SUBLIST.java
@@ -33,6 +33,8 @@ import java.util.Map.Entry;
  * returns the list of elements from the lesser index to the bigger index (included).
  * If the parameter list contains more than two indices, the result of SUBLIST
  * contains all the elements at the specified indices, with possible duplicates.
+ * If, instead of the parameter list, there are number, they are considered to define
+ * a range. From top to bottom: step (optional), end(optional), start
  */
 public class SUBLIST extends NamedWarpScriptFunction implements WarpScriptStackFunction {
   
@@ -42,29 +44,62 @@ public class SUBLIST extends NamedWarpScriptFunction implements WarpScriptStackF
   
   @Override
   public Object apply(WarpScriptStack stack) throws WarpScriptException {
-    Object indices = stack.pop();
-    
-    Object list = stack.pop();
+    Object o;
+    List indices = null;
+    List elements = null;
+    ArrayList<Long> longParams = new ArrayList<Long>();
 
-    if (!(indices instanceof List) || !(list instanceof List)) {
-      throw new WarpScriptException(getName() + " expects a list of indices on top of the stack and will operate on the list below it.");
+    // Get the 4 elements on top of the stack or until a list is found.
+    // After this, either indices is null or longParams contains at least one long.
+    for (int i = 0; i < 4; i++) {
+      o = stack.pop();
+
+      if (o instanceof List) {
+        if (0 == i) { // No range defined as numbers
+          indices = (List) o;
+          o = stack.pop();
+          if (o instanceof List) {
+            elements = (List) o;
+          } else {
+            throw new WarpScriptException(getName() + " expects a list of indices on top of the stack and will operate on the list below it.");
+          }
+        } else {
+          elements = (List) o;
+        }
+        break;
+      } else if (o instanceof Number) {
+        longParams.add(0, ((Number) o).longValue());
+      } else {
+        throw new WarpScriptException(getName() + " expects a list of indices on top of the stack or a start end step and will operate on the list below it.");
+      }
+    }
+
+    // elements can be null if the 4 elements on top of the stack were numbers.
+    if (null == elements) {
+      throw new WarpScriptException(getName() + " expects a list of indices on top of the stack or a start end step and will operate on the list below it.");
     }
     
-    List<Object> lindices = (List) indices;
-    List<Object> elements = (List) list;
-    
     List<Object> sublist = new ArrayList<Object>();
-    
-    if (2 == lindices.size()) {
-      Object a = lindices.get(0);
-      Object b = lindices.get(1);
-      
-      if (!(a instanceof Long) || !(b instanceof Long)) {
-        throw new WarpScriptException(getName() + " expects a list of indices which are numeric integers.");
+
+    if (null == indices || 2 == indices.size()) { // Range definition
+      int la, lb, step;
+
+      if(null == indices){
+        la = longParams.size() > 0 ? longParams.get(0).intValue() : 0;
+        lb = longParams.size() > 1 ? longParams.get(1).intValue() : -1;
+        step = longParams.size() > 2 ? longParams.get(2).intValue() : 1;
+      } else {
+        Object a = indices.get(0);
+        Object b = indices.get(1);
+        step = 1;
+
+        if (!(a instanceof Long) || !(b instanceof Long)) {
+          throw new WarpScriptException(getName() + " expects a list of indices which are numeric integers.");
+        }
+
+        la = ((Long) a).intValue();
+        lb = ((Long) b).intValue();
       }
-      
-      int la = ((Long) a).intValue();
-      int lb = ((Long) b).intValue();
 
       if (la < 0) {
         la = elements.size() + la;
@@ -103,20 +138,20 @@ public class SUBLIST extends NamedWarpScriptFunction implements WarpScriptStackF
       if (la < lb) {
         if (la < elements.size()) {
           lb = Math.min(elements.size() - 1, lb);
-          for (int i = la; i <= lb; i++) {
+          for (int i = la; i <= lb; i += step) {
             sublist.add(elements.get(i));
           }                  
         }
       } else {
         if (lb < elements.size()) {
           la = Math.min(elements.size() - 1, la);
-          for (int i = lb; i <= la; i++) {
+          for (int i = lb; i <= la; i += step) {
             sublist.add(elements.get(i));
           }                            
         }
       }
-    } else {
-      for (Object index: lindices) {
+    } else { // Individual elements selection
+      for (Object index: indices) {
         if (!(index instanceof Long)) {
           throw new WarpScriptException(getName() + " expects a list of indices which are numeric integers.");
         }
