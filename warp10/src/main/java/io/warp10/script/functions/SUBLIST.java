61ce5872095e205154a28415d82f1a10150ad24e
Fabien Tencé <fabien.tence@senx.io>
2019-04-04 12:02:15 +0200

    Make SUBLIST new signature work with negative steps

--- warp10/src/main/java/io/warp10/script/functions/SUBLIST.java
+++ warp10/src/main/java/io/warp10/script/functions/SUBLIST.java
@@ -17,15 +17,12 @@
 package io.warp10.script.functions;
 
 import io.warp10.script.NamedWarpScriptFunction;
-import io.warp10.script.WarpScriptStackFunction;
 import io.warp10.script.WarpScriptException;
 import io.warp10.script.WarpScriptStack;
+import io.warp10.script.WarpScriptStackFunction;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 /**
  * Create a new list with the elements whose indices are in the parameter list.
@@ -37,17 +34,17 @@ import java.util.Map.Entry;
  * a range. From top to bottom: step (optional), end(optional), start
  */
 public class SUBLIST extends NamedWarpScriptFunction implements WarpScriptStackFunction {
-  
+
   public SUBLIST(String name) {
     super(name);
   }
-  
+
   @Override
   public Object apply(WarpScriptStack stack) throws WarpScriptException {
     Object o;
     List indices = null;
     List elements = null;
-    ArrayList<Long> longParams = new ArrayList<Long>();
+    ArrayList<Integer> intParams = new ArrayList<Integer>();
 
     // Get the 4 elements on top of the stack or until a list is found.
     // After this, either indices is null or longParams contains at least one long.
@@ -68,7 +65,12 @@ public class SUBLIST extends NamedWarpScriptFunction implements WarpScriptStackF
         }
         break;
       } else if (o instanceof Number) {
-        longParams.add(0, ((Number) o).longValue());
+        try {
+          intParams.add(0, Math.toIntExact(((Number) o).longValue())); // Prepend the int parameter
+        }
+        catch(ArithmeticException ae){
+          throw new WarpScriptException(getName() + " expects number parameters to be ints.", ae);
+        }
       } else {
         throw new WarpScriptException(getName() + " expects a list of indices on top of the stack or a start end step and will operate on the list below it.");
       }
@@ -78,28 +80,65 @@ public class SUBLIST extends NamedWarpScriptFunction implements WarpScriptStackF
     if (null == elements) {
       throw new WarpScriptException(getName() + " expects a list of indices on top of the stack or a start end step and will operate on the list below it.");
     }
-    
+
     List<Object> sublist = new ArrayList<Object>();
 
-    if (null == indices || 2 == indices.size()) { // Range definition
-      int la, lb, step;
+    if (null == indices) { // Range definition with intParams
+      // If indices is null, there is at least one parameter in longParams: start.
+      int start = intParams.get(0);
+      int end = intParams.size() > 1 ? intParams.get(1) : -1; // Defaults to end of the list
 
-      if(null == indices){
-        la = longParams.size() > 0 ? longParams.get(0).intValue() : 0;
-        lb = longParams.size() > 1 ? longParams.get(1).intValue() : -1;
-        step = longParams.size() > 2 ? longParams.get(2).intValue() : 1;
-      } else {
-        Object a = indices.get(0);
-        Object b = indices.get(1);
-        step = 1;
-
-        if (!(a instanceof Long) || !(b instanceof Long)) {
-          throw new WarpScriptException(getName() + " expects a list of indices which are numeric integers.");
-        }
-
-        la = ((Long) a).intValue();
-        lb = ((Long) b).intValue();
+      if (start < 0) {
+        start += elements.size();
       }
+      if (end < 0) {
+        end += elements.size();
+      }
+
+      start = Math.max(0, Math.min(elements.size() -1 ,start));
+      end = Math.max(0, Math.min(elements.size() - 1, end));
+
+      int step = 1;
+      if (intParams.size() > 2) {
+        step = intParams.get(2);
+      } else {
+        if (start > end) {
+          step = -1;
+        }
+        // else step is already 1
+      }
+
+      if (0 == step) {
+        throw new WarpScriptException(getName() + " expects the step parameter to be a strictly positive or negative integer.");
+      } else if (step > 0) {
+        if (start > end) {
+          throw new WarpScriptException(getName() + " expects start to be before end when step is positive.");
+        }
+      } else {
+        if (end > start) {
+          throw new WarpScriptException(getName() + " expects start to be after end when step is negative.");
+        }
+      }
+
+      if (step > 0) {
+        for (int i = start; i <= end; i += step) {
+          sublist.add(elements.get(i));
+        }
+      } else {
+        for (int i = start; i >= end; i += step) {
+          sublist.add(elements.get(i));
+        }
+      }
+    } else if (2 == indices.size()) { // Range definition with indices
+      Object a = indices.get(0);
+      Object b = indices.get(1);
+
+      if (!(a instanceof Long) || !(b instanceof Long)) {
+        throw new WarpScriptException(getName() + " expects a list of indices which are numeric integers.");
+      }
+
+      int la = ((Long) a).intValue();
+      int lb = ((Long) b).intValue();
 
       if (la < 0) {
         la = elements.size() + la;
@@ -115,12 +154,12 @@ public class SUBLIST extends NamedWarpScriptFunction implements WarpScriptStackF
         la = Math.max(0, la);
         lb = Math.max(0, lb);
       }
-      
+
       //
       // If at least one of the bounds is included in the list indices,
       // fix the other one
       //
-      
+
       if (la < elements.size() || lb < elements.size()) {
         if (la >= elements.size()) {
           la = elements.size() - 1;
@@ -132,22 +171,22 @@ public class SUBLIST extends NamedWarpScriptFunction implements WarpScriptStackF
           lb = elements.size() - 1;
         } else if (lb < -1 * elements.size()) {
           lb = -1 * elements.size();
-        }        
+        }
       }
 
       if (la < lb) {
         if (la < elements.size()) {
           lb = Math.min(elements.size() - 1, lb);
-          for (int i = la; i <= lb; i += step) {
+          for (int i = la; i <= lb; i++) {
             sublist.add(elements.get(i));
-          }                  
+          }
         }
       } else {
         if (lb < elements.size()) {
           la = Math.min(elements.size() - 1, la);
-          for (int i = lb; i <= la; i += step) {
+          for (int i = lb; i <= la; i++) {
             sublist.add(elements.get(i));
-          }                            
+          }
         }
       }
     } else { // Individual elements selection
@@ -155,13 +194,13 @@ public class SUBLIST extends NamedWarpScriptFunction implements WarpScriptStackF
         if (!(index instanceof Long)) {
           throw new WarpScriptException(getName() + " expects a list of indices which are numeric integers.");
         }
-        
+
         int idx = ((Long) index).intValue();
-        
+
         if (idx >= elements.size() || (idx < -1 * elements.size())) {
           throw new WarpScriptException(getName() + " reported an out of bound index.");
         }
-        
+
         if (idx >= 0) {
           sublist.add(elements.get(idx));
         } else {
@@ -169,7 +208,7 @@ public class SUBLIST extends NamedWarpScriptFunction implements WarpScriptStackF
         }
       }
     }
-    
+
     stack.push(sublist);
 
     return stack;
