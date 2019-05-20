c90ec2b9d17503f71ab95feefee4aa888a8d87b7
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2019-05-20 11:42:01 +0200

    Initial commit of SHAPE and RESHAPE

--- warp10/src/main/java/io/warp10/script/WarpScriptLib.java
+++ warp10/src/main/java/io/warp10/script/WarpScriptLib.java
@@ -903,6 +903,8 @@ public class WarpScriptLib {
     addNamedWarpScriptFunction(new LTTB("TLTTB", true));
     addNamedWarpScriptFunction(new LOCATIONOFFSET("LOCATIONOFFSET"));
     addNamedWarpScriptFunction(new FLATTEN("FLATTEN"));
+    addNamedWarpScriptFunction(new RESHAPE("RESHAPE"));
+    addNamedWarpScriptFunction(new SHAPE("SHAPE"));
     addNamedWarpScriptFunction(new CORRELATE.Builder("CORRELATE"));
     addNamedWarpScriptFunction(new SORT("SORT"));
     addNamedWarpScriptFunction(new SORTBY("SORTBY"));
