00258e4bed0beb53d98b1b6115703c4c39cd4d2d
Steven GUEGUEN <steven.gueguen@senx.io>
2019-06-12 17:33:24 +0200

    Added support for multiple configuration files Use TokenGen instead of Worf

---
+++ etc/generate_crypto_key.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#
+#   Copyright 2018  SenX S.A.S.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import binascii
+import os
+
+cryptokeys = {
+  "warp.hash.class.secret" : 128,
+  "warp.hash.labels.secret" : 128,
+  "warp.hash.index.secret" : 128,
+  "warp.hash.token.secret" : 128,
+  "warp.hash.app.secret" : 128,
+  "warp.aes.token.secret" : 256,
+  "warp.aes.scripts.secret" : 256,
+  "warp.aes.metasets.secret" : 256,
+  "warp.aes.logging.secret" : 256,
+}
+
+for key, value in cryptokeys.items():
+  print(key + " = hex:" + binascii.hexlify(os.urandom(int(value / 8))).decode("utf-8"))
