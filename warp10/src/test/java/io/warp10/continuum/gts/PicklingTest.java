3cce32d3855f46dc694e4bfefafdc914fb1500fd
jc <jean-charles.vialatte@senx.io>
2019-07-31 17:53:11 +0200

    removed line separators

--- warp10/src/test/java/io/warp10/continuum/gts/PicklingTest.java
+++ warp10/src/test/java/io/warp10/continuum/gts/PicklingTest.java
@@ -47,21 +47,21 @@ public class PicklingTest {
   public void testPickledTS() throws Exception {
 
     stack.getSymbolTable().clear();
-    stack.exec("[ 1 10 <% s %> FOR ] [] [] [] [ 1 10 <% DROP RAND %> FOR ] MAKEGTS" + System.lineSeparator());
-    stack.exec("'gts' STORE" + System.lineSeparator());
-    stack.exec("$gts 'gts' RENAME { 'k1' 'v1' 'k2' 'v2' } RELABEL { 'k3' 'v3' 'k4' 'v4' } SETATTRIBUTES" + System.lineSeparator());
-    stack.exec("->PICKLE PICKLE-> 'map' STORE" + System.lineSeparator());
+    stack.exec("[ 1 10 <% s %> FOR ] [] [] [] [ 1 10 <% DROP RAND %> FOR ] MAKEGTS");
+    stack.exec("'gts' STORE");
+    stack.exec("$gts 'gts' RENAME { 'k1' 'v1' 'k2' 'v2' } RELABEL { 'k3' 'v3' 'k4' 'v4' } SETATTRIBUTES");
+    stack.exec("->PICKLE PICKLE-> 'map' STORE");
 
     //
     // Asserts
     //
 
-    stack.exec("$map SIZE 5 == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'classname' GET 'gts' == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'labels' GET ->JSON $gts LABELS ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'attributes' GET ->JSON $gts ATTRIBUTES ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'timestamps' GET ->JSON $gts TICKS ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'values' GET ->JSON $gts VALUES ->JSON == ASSERT" + System.lineSeparator());
+    stack.exec("$map SIZE 5 == ASSERT" );
+    stack.exec("$map 'classname' GET 'gts' == ASSERT" );
+    stack.exec("$map 'labels' GET ->JSON $gts LABELS ->JSON == ASSERT" );
+    stack.exec("$map 'attributes' GET ->JSON $gts ATTRIBUTES ->JSON == ASSERT" );
+    stack.exec("$map 'timestamps' GET ->JSON $gts TICKS ->JSON == ASSERT" );
+    stack.exec("$map 'values' GET ->JSON $gts VALUES ->JSON == ASSERT" );
     stack.exec("DEPTH 0 == ASSERT");
   }
 
@@ -69,23 +69,23 @@ public class PicklingTest {
   public void testPickledGTS() throws Exception {
 
     stack.getSymbolTable().clear();
-    stack.exec("[ 1 10 <% s %> FOR ] [ 1 10 <% us %> FOR ] [ 1 10 <% us %> FOR ] [] [ 1 10 <% DROP RAND %> FOR ] MAKEGTS" + System.lineSeparator());
-    stack.exec("'gts' STORE" + System.lineSeparator());
-    stack.exec("$gts 'gts' RENAME { 'k1' 'v1' 'k2' 'v2' } RELABEL { 'k3' 'v3' 'k4' 'v4' } SETATTRIBUTES" + System.lineSeparator());
-    stack.exec("->PICKLE PICKLE-> 'map' STORE" + System.lineSeparator());
+    stack.exec("[ 1 10 <% s %> FOR ] [ 1 10 <% us %> FOR ] [ 1 10 <% us %> FOR ] [] [ 1 10 <% DROP RAND %> FOR ] MAKEGTS" );
+    stack.exec("'gts' STORE" );
+    stack.exec("$gts 'gts' RENAME { 'k1' 'v1' 'k2' 'v2' } RELABEL { 'k3' 'v3' 'k4' 'v4' } SETATTRIBUTES" );
+    stack.exec("->PICKLE PICKLE-> 'map' STORE" );
 
     //
     // Asserts
     //
 
-    stack.exec("$map SIZE 7 == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'classname' GET 'gts' == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'labels' GET ->JSON $gts LABELS ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'attributes' GET ->JSON $gts ATTRIBUTES ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'timestamps' GET ->JSON $gts TICKS ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'values' GET ->JSON $gts VALUES ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'latitudes' GET ->JSON $gts LOCATIONS DROP ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'longitudes' GET ->JSON $gts LOCATIONS SWAP DROP ->JSON == ASSERT" + System.lineSeparator());
+    stack.exec("$map SIZE 7 == ASSERT" );
+    stack.exec("$map 'classname' GET 'gts' == ASSERT" );
+    stack.exec("$map 'labels' GET ->JSON $gts LABELS ->JSON == ASSERT" );
+    stack.exec("$map 'attributes' GET ->JSON $gts ATTRIBUTES ->JSON == ASSERT" );
+    stack.exec("$map 'timestamps' GET ->JSON $gts TICKS ->JSON == ASSERT" );
+    stack.exec("$map 'values' GET ->JSON $gts VALUES ->JSON == ASSERT" );
+    stack.exec("$map 'latitudes' GET ->JSON $gts LOCATIONS DROP ->JSON == ASSERT" );
+    stack.exec("$map 'longitudes' GET ->JSON $gts LOCATIONS SWAP DROP ->JSON == ASSERT" );
     stack.exec("DEPTH 0 == ASSERT");
   }
 
@@ -93,24 +93,24 @@ public class PicklingTest {
   public void testPickledGTSwithElevation() throws Exception {
 
     stack.getSymbolTable().clear();
-    stack.exec("[ 1 10 <% s %> FOR ] [ 1 10 <% us %> FOR ] [ 1 10 <% us %> FOR ] [ 1 10 <% ms %> FOR ] [ 1 10 <% DROP RAND %> FOR ] MAKEGTS" + System.lineSeparator());
-    stack.exec("'gts' STORE" + System.lineSeparator());
-    stack.exec("$gts 'gts' RENAME { 'k1' 'v1' 'k2' 'v2' } RELABEL { 'k3' 'v3' 'k4' 'v4' } SETATTRIBUTES" + System.lineSeparator());
-    stack.exec("->PICKLE PICKLE-> 'map' STORE" + System.lineSeparator());
+    stack.exec("[ 1 10 <% s %> FOR ] [ 1 10 <% us %> FOR ] [ 1 10 <% us %> FOR ] [ 1 10 <% ms %> FOR ] [ 1 10 <% DROP RAND %> FOR ] MAKEGTS" );
+    stack.exec("'gts' STORE" );
+    stack.exec("$gts 'gts' RENAME { 'k1' 'v1' 'k2' 'v2' } RELABEL { 'k3' 'v3' 'k4' 'v4' } SETATTRIBUTES" );
+    stack.exec("->PICKLE PICKLE-> 'map' STORE" );
 
     //
     // Asserts
     //
 
-    stack.exec("$map SIZE 8 == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'classname' GET 'gts' == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'labels' GET ->JSON $gts LABELS ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'attributes' GET ->JSON $gts ATTRIBUTES ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'timestamps' GET ->JSON $gts TICKS ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'values' GET ->JSON $gts VALUES ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'latitudes' GET ->JSON $gts LOCATIONS DROP ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'longitudes' GET ->JSON $gts LOCATIONS SWAP DROP ->JSON == ASSERT" + System.lineSeparator());
-    stack.exec("$map 'elevations' GET ->JSON $gts ELEVATIONS ->JSON == ASSERT" + System.lineSeparator());
+    stack.exec("$map SIZE 8 == ASSERT" );
+    stack.exec("$map 'classname' GET 'gts' == ASSERT" );
+    stack.exec("$map 'labels' GET ->JSON $gts LABELS ->JSON == ASSERT" );
+    stack.exec("$map 'attributes' GET ->JSON $gts ATTRIBUTES ->JSON == ASSERT" );
+    stack.exec("$map 'timestamps' GET ->JSON $gts TICKS ->JSON == ASSERT" );
+    stack.exec("$map 'values' GET ->JSON $gts VALUES ->JSON == ASSERT" );
+    stack.exec("$map 'latitudes' GET ->JSON $gts LOCATIONS DROP ->JSON == ASSERT" );
+    stack.exec("$map 'longitudes' GET ->JSON $gts LOCATIONS SWAP DROP ->JSON == ASSERT" );
+    stack.exec("$map 'elevations' GET ->JSON $gts ELEVATIONS ->JSON == ASSERT" );
     stack.exec("DEPTH 0 == ASSERT");
   }
 }
