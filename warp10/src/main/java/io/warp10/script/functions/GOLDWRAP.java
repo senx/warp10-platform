dd0215e0707bb9ed2abccf6a192328675d54f17e
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2019-09-10 14:41:21 +0200

    Added fast path

--- warp10/src/main/java/io/warp10/script/functions/GOLDWRAP.java
+++ warp10/src/main/java/io/warp10/script/functions/GOLDWRAP.java
@@ -52,10 +52,14 @@ public class GOLDWRAP extends ElementOrListStackFunction {
       public Object applyOnElement(Object element) throws WarpScriptException {
         GTSEncoder encoder = null;
         
+        boolean sortedEncoder = false;
+        
         try {
           if (element instanceof GeoTimeSerie) {
             encoder = new GTSEncoder(0L);
-            encoder.encode((GeoTimeSerie) element);
+            GTSHelper.fullsort((GeoTimeSerie) element, false);
+            sortedEncoder = true;
+            encoder.encodeOptimized((GeoTimeSerie) element);
           } else if (element instanceof GTSEncoder) {
             encoder = (GTSEncoder) element;
           } else if (element instanceof String || element instanceof byte[]) {
@@ -74,91 +78,100 @@ public class GOLDWRAP extends ElementOrListStackFunction {
             throw new WarpScriptException(getName() + " can only be applied to Geo Time Series™, GTS Encoders or wrapped instances of those types.");
           } 
           
-          //
-          // Split the encoder in 5 GTS, one per type, in this order:
-          //
-          // LONG, DOUBLE, BOOLEAN, STRING, BINARY
-          //
+          GTSEncoder enc = null;
           
-          GeoTimeSerie[] gts = new GeoTimeSerie[5];
+          if (sortedEncoder) {
+            // We had a single GeoTimeSerie instance that we sorted and encoded
+            // as an optimized encoder, so take a fastpath!
+            enc = encoder;
+          } else {
+            //
+            // Split the encoder in 5 GTS, one per type, in this order:
+            //
+            // LONG, DOUBLE, BOOLEAN, STRING, BINARY
+            //
+            
+            GeoTimeSerie[] gts = new GeoTimeSerie[5];
 
-          for (int i = 0; i < gts.length; i++) {
-            gts[i] = new GeoTimeSerie();
-          }
-          
-          GTSDecoder decoder = encoder.getDecoder();
-          
-          // Populate the 5 GTS
-          while (decoder.next()) {
-            long ts = decoder.getTimestamp();
-            long location = decoder.getLocation();
-            long elevation = decoder.getElevation();
-            Object value = decoder.getBinaryValue();
-            
-            if (value instanceof Long) {
-              GTSHelper.setValue(gts[0], ts, location, elevation, value, false);
-            } else if (value instanceof Double || value instanceof BigDecimal) {
-              GTSHelper.setValue(gts[1], ts, location, elevation, value, false);          
-            } else if (value instanceof Boolean) {
-              GTSHelper.setValue(gts[2], ts, location, elevation, value, false);
-            } else if (value instanceof String) {
-              GTSHelper.setValue(gts[3], ts, location, elevation, value, false);
-            } else if (value instanceof byte[]) {
-              GTSHelper.setValue(gts[4], ts, location, elevation, value, false);
-            }
-          }
-          
-          // Sort the 5 GTS using fullsort so we get a deterministic order
-          // in the presence of duplicate ticks
-          
-          for (int i = 0; i < gts.length; i++) {
-            GTSHelper.fullsort(gts[i], false);
-          }
-          
-          // Now merge the GTS in time order with the type precedence of the 'gts' array
-          
-          GTSEncoder enc = new GTSEncoder(0L);
-          enc.setMetadata(encoder.getMetadata());
-          
-          int[] idx = new int[gts.length];
-          
-          while (true) {
-            // Determine the next GTS to add from its timestamp, lowest first
-            int gtsidx = -1;
-            
-            long ts = Long.MAX_VALUE;
             for (int i = 0; i < gts.length; i++) {
-              if (idx[i] >= GTSHelper.nvalues(gts[i])) {
-                continue;
-              }
-              long tick = GTSHelper.tickAtIndex(gts[i], idx[i]);
-              if (-1 == gtsidx || tick < ts) {
-                gtsidx = i;
-                ts = tick;
+              gts[i] = new GeoTimeSerie();
+            }
+            
+            GTSDecoder decoder = encoder.getDecoder();
+            
+            // Populate the 5 GTS
+            while (decoder.next()) {
+              long ts = decoder.getTimestamp();
+              long location = decoder.getLocation();
+              long elevation = decoder.getElevation();
+              Object value = decoder.getBinaryValue();
+              
+              if (value instanceof Long) {
+                GTSHelper.setValue(gts[0], ts, location, elevation, value, false);
+              } else if (value instanceof Double || value instanceof BigDecimal) {
+                GTSHelper.setValue(gts[1], ts, location, elevation, value, false);          
+              } else if (value instanceof Boolean) {
+                GTSHelper.setValue(gts[2], ts, location, elevation, value, false);
+              } else if (value instanceof String) {
+                GTSHelper.setValue(gts[3], ts, location, elevation, value, false);
+              } else if (value instanceof byte[]) {
+                GTSHelper.setValue(gts[4], ts, location, elevation, value, false);
               }
             }
             
-            if (-1 == gtsidx) {
-              break;
+            // Sort the 5 GTS using fullsort so we get a deterministic order
+            // in the presence of duplicate ticks
+            
+            for (int i = 0; i < gts.length; i++) {
+              GTSHelper.fullsort(gts[i], false);
             }
             
-            do {
-              long location = GTSHelper.locationAtIndex(gts[gtsidx], idx[gtsidx]);
-              long elevation = GTSHelper.elevationAtIndex(gts[gtsidx], idx[gtsidx]);
-              Object value = GTSHelper.valueAtIndex(gts[gtsidx], idx[gtsidx]);
+            // Now merge the GTS in time order with the type precedence of the 'gts' array
+            
+            enc = new GTSEncoder(0L);
+            enc.setMetadata(encoder.getMetadata());
+            
+            int[] idx = new int[gts.length];
+            
+            while (true) {
+              // Determine the next GTS to add from its timestamp, lowest first
+              int gtsidx = -1;
               
-              if (4 == gtsidx) { // BINARY
-                value = value.toString().getBytes(StandardCharsets.ISO_8859_1);
-              } else if (2 == gtsidx) { // DOUBLE
-                // Attempt to optimize the value
-                value = GTSEncoder.optimizeValue(value);
+              long ts = Long.MAX_VALUE;
+              for (int i = 0; i < gts.length; i++) {
+                if (idx[i] >= GTSHelper.nvalues(gts[i])) {
+                  continue;
+                }
+                long tick = GTSHelper.tickAtIndex(gts[i], idx[i]);
+                if (-1 == gtsidx || tick < ts) {
+                  gtsidx = i;
+                  ts = tick;
+                }
               }
               
-              enc.addValue(ts, location, elevation, value);
+              if (-1 == gtsidx) {
+                break;
+              }
               
-              idx[gtsidx]++;
-            } while (idx[gtsidx] < GTSHelper.nvalues(gts[gtsidx]) && GTSHelper.tickAtIndex(gts[gtsidx], idx[gtsidx]) == ts);            
-          }         
+              do {
+                long location = GTSHelper.locationAtIndex(gts[gtsidx], idx[gtsidx]);
+                long elevation = GTSHelper.elevationAtIndex(gts[gtsidx], idx[gtsidx]);
+                Object value = GTSHelper.valueAtIndex(gts[gtsidx], idx[gtsidx]);
+                
+                if (4 == gtsidx) { // BINARY
+                  value = value.toString().getBytes(StandardCharsets.ISO_8859_1);
+                } else if (2 == gtsidx) { // DOUBLE
+                  // Attempt to optimize the value
+                  value = GTSEncoder.optimizeValue(value);
+                }
+                
+                enc.addValue(ts, location, elevation, value);
+                
+                idx[gtsidx]++;
+              } while (idx[gtsidx] < GTSHelper.nvalues(gts[gtsidx]) && GTSHelper.tickAtIndex(gts[gtsidx], idx[gtsidx]) == ts);            
+            }                     
+          }
+          
           GTSWrapper wrapper = GTSWrapperHelper.fromGTSEncoderToGTSWrapper(enc, true, 1.0D, Integer.MAX_VALUE);
           TSerializer ser = new TSerializer(new TCompactProtocol.Factory());
           byte[] bytes = ser.serialize(wrapper);
