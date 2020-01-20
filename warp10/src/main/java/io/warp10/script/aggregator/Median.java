c1b12779c52e1c7034fc5945a6e55bec271f2794
pi-r-p <pierre.papin@senx.io>
2020-01-20 17:02:04 +0100

    fix median: one location is better than nothing.

--- warp10/src/main/java/io/warp10/script/aggregator/Median.java
+++ warp10/src/main/java/io/warp10/script/aggregator/Median.java
@@ -62,7 +62,7 @@ public class Median extends NamedWarpScriptFunction implements WarpScriptAggrega
       inputHasDouble |= (v instanceof Double);
     }
 
-    if (nullCounter != 0 && this.forbidNulls) {
+    if (0 != nullCounter && this.forbidNulls) {
       throw new WarpScriptException(this.getName() + " cannot compute median of null values.");
     }
 
@@ -72,7 +72,9 @@ public class Median extends NamedWarpScriptFunction implements WarpScriptAggrega
     }
     //
     // sort indices from values, null at the end of the sorted array.
+    // a null is considered bigger than any other value.
     //
+    final String functionName = this.getName();
     Arrays.sort(indices, new Comparator<Integer>() {
       @Override
       public int compare(Integer idx1, Integer idx2) {
@@ -83,12 +85,12 @@ public class Median extends NamedWarpScriptFunction implements WarpScriptAggrega
         } else if (values[idx1] instanceof Number && values[idx2] instanceof Number) {
           return EQ.compare((Number) values[idx1], (Number) values[idx2]);
         } else {
-          throw new RuntimeException("MEDIAN can only operate on numeric Geo Time Series.");
+          throw new RuntimeException(functionName + " can only operate on numeric Geo Time Series.");
         }
       }
     });
 
-    long location = 0;
+    long location = GeoTimeSerie.NO_LOCATION;
     long elevation = GeoTimeSerie.NO_ELEVATION;
     Object median;
 
@@ -107,17 +109,26 @@ public class Median extends NamedWarpScriptFunction implements WarpScriptAggrega
         // even number of non null values, return mean of both values.
         // If there is a location for both points, return centroid of locations
         // If there is an elevation for both points, return mean of elevations
+        // If there is location or elevation for just one point, return this point. Better than nothing.
         //
         int low = indices[nonNullLength / 2 - 1];
         int high = indices[nonNullLength / 2];
         median = (((Number) values[low]).doubleValue() + ((Number) values[high]).doubleValue()) / 2.0D;
         if (GeoTimeSerie.NO_ELEVATION != elevations[low] && GeoTimeSerie.NO_ELEVATION != elevations[high]) {
           elevation = (elevations[low] + elevations[high]) / 2;
+        } else if (GeoTimeSerie.NO_ELEVATION != elevations[low]) {
+          elevation = elevations[low];
+        } else if (GeoTimeSerie.NO_ELEVATION != elevations[high]) {
+          elevation = elevations[high];
         }
         if (GeoTimeSerie.NO_LOCATION != locations[low] && GeoTimeSerie.NO_LOCATION != locations[high]) {
           long[] xyLow = GeoXPLib.xyFromGeoXPPoint(locations[low]);
           long[] xyHigh = GeoXPLib.xyFromGeoXPPoint(locations[high]);
           location = GeoXPLib.toGeoXPPoint((xyLow[0] + xyHigh[0]) / 2, (xyLow[1] + xyHigh[1]) / 2);
+        } else if (GeoTimeSerie.NO_LOCATION != locations[low]) {
+          location = locations[low];
+        } else if (GeoTimeSerie.NO_LOCATION != locations[high]) {
+          location = locations[high];
         }
 
       } else {
