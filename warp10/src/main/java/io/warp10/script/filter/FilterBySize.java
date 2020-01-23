eb353bda4794e2345b47131aa8d5f0445862e5aa
Jean-Charles Vialatte <jean-charles.vialatte@senx.io>
2020-01-23 14:49:33 +0100

    fix indent

--- warp10/src/main/java/io/warp10/script/filter/FilterBySize.java
+++ warp10/src/main/java/io/warp10/script/filter/FilterBySize.java
@@ -29,68 +29,68 @@ import java.util.List;
 import java.util.Map;
 
 public class FilterBySize extends NamedWarpScriptFunction implements WarpScriptFilterFunction {
-    static private final String MIN = "min";
-    static private final String MAX = "max";
+  static private final String MIN = "min";
+  static private final String MAX = "max";
 
-    private final Long min;
-    private final Long max;
+  private final Long min;
+  private final Long max;
 
-    public static class Builder extends FormattedWarpScriptFunction {
+  public static class Builder extends FormattedWarpScriptFunction {
 
-        private final Arguments args;
+    private final Arguments args;
 
-        public Builder(String name) {
-            super(name);
+    public Builder(String name) {
+      super(name);
 
-            args = new ArgumentsBuilder()
-                    .addArgument(Long.class, MIN, "The minimum size.")
-                    .addArgument(Long.class, MAX, "The maximum size.")
-                    .build();
-        }
-
-        @Override
-        protected Arguments getArguments() {
-            return args;
-        }
-
-        @Override
-        protected WarpScriptStack apply(Map<String, Object> formattedArgs, WarpScriptStack stack) throws WarpScriptException {
-            Long min = (Long) formattedArgs.get(MIN);
-            Long max = (Long) formattedArgs.get(MAX);
-            stack.push(new FilterBySize(getName(), min, max));
-            return null;
-        }
-    }
-
-    public FilterBySize(String name, Long min, Long max) throws WarpScriptException {
-        super(name);
-        this.min = min;
-        this.max = max;
+      args = new ArgumentsBuilder()
+        .addArgument(Long.class, MIN, "The minimum size.")
+        .addArgument(Long.class, MAX, "The maximum size.")
+        .build();
     }
 
     @Override
-    public List<GeoTimeSerie> filter(Map<String, String> labels, List<GeoTimeSerie>... series) throws WarpScriptException {
-
-        List<GeoTimeSerie> retained = new ArrayList<GeoTimeSerie>();
-
-        for (List<GeoTimeSerie> serie: series) {
-            for (GeoTimeSerie gts: serie) {
-                if (gts.size() >= min && gts.size() <= max) {
-                    retained.add(gts);
-                }
-            }
-        }
-
-        return retained;
+    protected Arguments getArguments() {
+      return args;
     }
 
     @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(StackUtils.toString(min));
-        sb.append(StackUtils.toString(max));
-        sb.append(" ");
-        sb.append(this.getName());
-        return sb.toString();
+    protected WarpScriptStack apply(Map<String, Object> formattedArgs, WarpScriptStack stack) throws WarpScriptException {
+      Long min = (Long) formattedArgs.get(MIN);
+      Long max = (Long) formattedArgs.get(MAX);
+      stack.push(new FilterBySize(getName(), min, max));
+      return null;
     }
+  }
+
+  public FilterBySize(String name, Long min, Long max) throws WarpScriptException {
+    super(name);
+    this.min = min;
+    this.max = max;
+  }
+
+  @Override
+  public List<GeoTimeSerie> filter(Map<String, String> labels, List<GeoTimeSerie>... series) throws WarpScriptException {
+
+    List<GeoTimeSerie> retained = new ArrayList<GeoTimeSerie>();
+
+    for (List<GeoTimeSerie> serie: series) {
+      for (GeoTimeSerie gts: serie) {
+        if (gts.size() >= min && gts.size() <= max) {
+          retained.add(gts);
+        }
+      }
+    }
+
+    return retained;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(StackUtils.toString(min));
+    sb.append(StackUtils.toString(max));
+    sb.append(" ");
+    sb.append(this.getName());
+    return sb.toString();
+  }
 }
