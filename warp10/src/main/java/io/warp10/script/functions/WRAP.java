e115881d4fcbfddfd50e6bfa3da2770e863ab7fd
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2020-04-10 07:41:22 +0200

    Fixed copyright notice and tm

--- warp10/src/main/java/io/warp10/script/functions/WRAP.java
+++ warp10/src/main/java/io/warp10/script/functions/WRAP.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2019  SenX S.A.S.
+//   Copyright 2019-2020  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -86,7 +86,7 @@ public class WRAP extends ElementOrListStackFunction {
             wrapper = GTSWrapperHelper.fromGTSEncoderToGTSWrapper((GTSEncoder) element, compress);
           }
         } else {
-          throw new WarpScriptException(getName() + " expects a Geo Time Series™ of a GTSEncoder or a list on top of the stack");
+          throw new WarpScriptException(getName() + " expects a Geo Time Series of a GTSEncoder or a list on top of the stack");
         }
 
         if (mv) {
