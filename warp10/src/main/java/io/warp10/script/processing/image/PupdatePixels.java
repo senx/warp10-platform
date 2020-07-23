68cb8015cceb29770b0b94fc0b8ca085dd7b737f
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2020-07-23 15:51:45 +0200

    Cleaned import. Fixed copyright notice

--- warp10/src/main/java/io/warp10/script/processing/image/PupdatePixels.java
+++ warp10/src/main/java/io/warp10/script/processing/image/PupdatePixels.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2018  SenX S.A.S.
+//   Copyright 2018-2020  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -16,15 +16,13 @@
 
 package io.warp10.script.processing.image;
 
-import io.warp10.script.NamedWarpScriptFunction;
-import io.warp10.script.WarpScriptStackFunction;
-import io.warp10.script.WarpScriptException;
-import io.warp10.script.WarpScriptStack;
-import io.warp10.script.processing.ProcessingUtil;
-
-import java.util.ArrayList;
 import java.util.List;
 
+import io.warp10.script.NamedWarpScriptFunction;
+import io.warp10.script.WarpScriptException;
+import io.warp10.script.WarpScriptStack;
+import io.warp10.script.WarpScriptStackFunction;
+import io.warp10.script.processing.ProcessingUtil;
 import processing.core.PGraphics;
 import processing.core.PImage;
 
