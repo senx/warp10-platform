cf668197948071dfc54415c59c5d4fb68679f04d
Jean-Charles Vialatte <jean-charles.vialatte@senx.io>
2020-12-18 19:33:59 +0100

    change anum

--- warp10/src/main/java/io/warp10/script/ext/http/HTTP.java
+++ warp10/src/main/java/io/warp10/script/ext/http/HTTP.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2018  SenX S.A.S.
+//   Copyright 2021  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -172,7 +172,7 @@ public class HTTP extends FormattedWarpScriptFunction {
     }
 
     HttpURLConnection conn = null;
-    
+
     List<Object> res = new ArrayList<Object>();
 
     try {
