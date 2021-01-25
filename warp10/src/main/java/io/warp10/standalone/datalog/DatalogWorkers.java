a9a96bd71ba6ca0071dda5e8842538036e9f2476
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2021-01-25 16:00:50 +0100

    Changed TCPDatalogConsumer to DatalogConsumer

--- warp10/src/main/java/io/warp10/standalone/datalog/DatalogWorkers.java
+++ warp10/src/main/java/io/warp10/standalone/datalog/DatalogWorkers.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2020  SenX S.A.S.
+//   Copyright 2020-2021  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -26,64 +26,64 @@ import io.warp10.continuum.store.StoreClient;
 import io.warp10.continuum.store.thrift.data.DatalogRecord;
 
 public class DatalogWorkers {
-  
+
   static class DatalogJob {
-    final TCPDatalogConsumer consumer;
+    final DatalogConsumer consumer;
     final String ref;
     final DatalogRecord record;
-    
-    public DatalogJob(TCPDatalogConsumer consumer, String ref, DatalogRecord record) {
+
+    public DatalogJob(DatalogConsumer consumer, String ref, DatalogRecord record) {
       this.consumer = consumer;
       this.ref = ref;
       this.record = record;
-    }    
+    }
   }
-  
+
   private static final LinkedBlockingQueue<DatalogJob> queues[];
   private static final DatalogWorker[] workers;
-  
+
   private static StoreClient store = null;
   private static DirectoryClient directory = null;
-  
+
   private static final int NUM_WORKERS = 8;
-  
+
   static {
     queues = new LinkedBlockingQueue[NUM_WORKERS];
     workers = new DatalogWorker[NUM_WORKERS];
-    
+
     for (int i = 0; i < NUM_WORKERS; i++) {
       queues[i] = new LinkedBlockingQueue<DatalogJob>();
-      workers[i] = new DatalogWorker(queues[i]);      
+      workers[i] = new DatalogWorker(queues[i]);
     }
   }
-  
+
   public static void init(StoreClient store, DirectoryClient directory) {
-    
+
     if (null != DatalogWorkers.store || null != DatalogWorkers.directory) {
       throw new RuntimeException("Store/Directory already set.");
     }
-    
+
     DatalogWorkers.store = store;
     DatalogWorkers.directory = directory;
   }
-  
-  public static void offer(TCPDatalogConsumer consumer, String ref, DatalogRecord record) throws IOException {
+
+  public static void offer(DatalogConsumer consumer, String ref, DatalogRecord record) throws IOException {
     //
     // Compute partitioning key
     //
 
     long classid = record.getMetadata().getClassId();
     long labelsid = record.getMetadata().getLabelsId();
-    
+
     int partkey = (int) (((classid << 16) & 0xFFFFL) | ((labelsid >>> 48) & 0xFFFFL));
-    
+
     DatalogJob job = new DatalogJob(consumer, ref, record);
-    
+
     if (!queues[partkey % NUM_WORKERS].offer(job)) {
       throw new IOException("Unable to offer record '" + ref + "'.");
     }
   }
-  
+
   public static StoreClient getStoreClient() {
     return DatalogWorkers.store;
   }
