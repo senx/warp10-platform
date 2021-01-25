8422cc60694e2e09aaf1d470f24ea4a1d2360210
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2021-01-25 16:01:16 +0100

    Added comment for sharding macro

--- warp10/src/main/java/io/warp10/standalone/datalog/TCPDatalogConsumer.java
+++ warp10/src/main/java/io/warp10/standalone/datalog/TCPDatalogConsumer.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2020  SenX S.A.S.
+//   Copyright 2020-2021  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -68,18 +68,18 @@ import io.warp10.sensision.Sensision;
  * Consumes a Datalog feed and updates a backend accordingly
  */
 public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
-  
+
   private static final String SENSISION_CLASS_DATALOG_IGNORED = "datalog.ignored";
   private static final String SENSISION_LABEL_CONSUMER = "consumer";
   private static final String SENSISION_LABEL_FEEDER = "feeder";
-  
+
   private WarpScriptStack stack;
-  
+
   private ECPrivateKey eccPrivate;
   private ECPublicKey eccPublic;
 
   private String id;
-  
+
   /**
    * Set of IDs whose messages are ignored
    */
@@ -89,14 +89,14 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
 
   private long[] CLASS_KEYS;
   private long[] LABELS_KEYS;
-  
+
   private String feeder;
-  
+
   /**
    * Optional encryption key
    */
   private byte[] AES_KEY;
-  
+
   /**
    * Lists of inflight, failed and successful commit refs
    */
@@ -108,23 +108,23 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
    * Path to the file where last committed offset is kept track of
    */
   private File offsetFile;
-  
+
   /**
    * Delay (in ms) between offset commits to offsetFile. 0 means write every commit.
    */
   private long offsetDelay;
   private long lastsync = 0;
-  
+
   private String suffix;
-  
+
   @Override
   public void run() {
-      
+
     String host = WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_FEEDER_HOST + suffix);
     int port = Integer.parseInt(WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_FEEDER_PORT + suffix));
-        
+
     String feederShardsSpec = WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_FEEDER_SHARDS + suffix);
-    
+
     long[] feederShards = null;
     int feederShardShift = Integer.parseInt(WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_FEEDER_SHARDSHIFT + suffix, "48"));
 
@@ -140,15 +140,15 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
         feederShards[i] = (((long) Integer.parseInt(subtokens[0])) << 32) | ((long) Integer.parseInt(subtokens[1]) & 0xFFFFFFFFL);
       }
     }
-    
+
     int[] modulus = null;
     int[] remainder = null;
-    
+
     String shardSpec = WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_SHARDS + suffix);
-    
+
     boolean hasShards = false;
     int shardShift = Integer.parseInt(WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_SHARDSHIFT + suffix, "48"));
-    
+
     if (null != shardSpec) {
       hasShards = true;
       String[] tokens = feederShardsSpec.split(",");
@@ -162,37 +162,37 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
         }
         modulus[i] = Integer.parseInt(subtokens[0]);
         remainder[i] = Integer.parseInt(subtokens[1]);
-      }      
+      }
     }
-    
+
     while(true) {
       Socket socket = null;
-      
+
       try {
         InetAddress addr = InetAddress.getByName(host);
-        
+
         socket = new Socket(addr, port);
 
         InputStream in = socket.getInputStream();
         OutputStream out = socket.getOutputStream();
-        
+
         //
         // Read welcome message
         //
-        
+
         byte[] bytes = DatalogHelper.readBlob(in, 0);
-        
+
         DatalogMessage msg = new DatalogMessage();
         DatalogHelper.deserialize(bytes, msg);
-        
+
         if (DatalogMessageType.WELCOME != msg.getType()) {
           throw new IOException("Invalid message type " + msg.getType().name() + ", aborting.");
         }
-        
+
         //
         // Compute signature and emit INIT message
         //
-        
+
         stack.clear();
         stack.push(Longs.toByteArray(msg.getNonce()));
         stack.push(Longs.toByteArray(msg.getTimestamp()));
@@ -205,48 +205,48 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
         stack.push(eccPrivate);
         ECSIGN.apply(stack);
         byte[] sig = (byte[]) stack.pop();
-        
+
         boolean encrypt = msg.isEncrypt();
-        
+
         if (encrypt) {
           //
           // Compute the encryption key using ECDH
           //
-          
+
           String feederpub = WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_FEEDER_ECC_PUBLIC + suffix);
-          
+
           String[] tokens = feederpub.split(":");
           Map<String,String> map = new HashMap<String,String>();
           map.put(Constants.KEY_CURVE, tokens[0]);
           map.put(Constants.KEY_Q, tokens[1]);
-            
+
           ECPublicKey feederPublic;
-          
+
           try {
             stack.push(map);
             new ECPUBLIC(WarpScriptLib.ECPUBLIC).apply(stack);
             feederPublic = (ECPublicKey) stack.pop();
           } catch (WarpScriptException wse) {
             throw new RuntimeException("Error extracting ECC public key.", wse);
-          }        
+          }
 
           long nonce = msg.getNonce();
           long timestamp = msg.getTimestamp();
-          
+
           //
           // Key is
           // ->LONGBYTES(SIPHASH(nonce,timestamp,secret),8)
           // + ->LONGBYTES(SIPHASH(timestamp,nonce,secret),8)
           // + ->LONGBYTES(SIPHASH(nonce,timestamp,CLONEREVERSE(secret)),8)
           // + ->LONGBYTES(SIPHASH(timestamp,nonce,CLONEREVERSE(secret)),8)
-          //          
+          //
 
           stack.clear();
           stack.push(eccPrivate);
           stack.push(feederPublic);
           new ECDH(WarpScriptLib.ECDH).apply(stack);
           byte[] secret = Hex.decode((String) stack.pop());
-          
+
           stack.clear();
           stack.push(secret);
           stack.push(nonce);
@@ -277,28 +277,28 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
           new ADD(WarpScriptLib.ADD).apply(stack);
           new ADD(WarpScriptLib.ADD).apply(stack);
           new ADD(WarpScriptLib.ADD).apply(stack);
-          
-          this.AES_KEY = (byte[]) stack.pop();          
+
+          this.AES_KEY = (byte[]) stack.pop();
         }
-        
+
         msg.clear();
         msg.setType(DatalogMessageType.INIT);
         msg.setId(this.id);
         msg.setSig(sig);
-        
+
         if (null != feederShards) {
           for (long shard: feederShards) {
             msg.addToShards(shard);
           }
           msg.setShardShift(feederShardShift);
         }
-        
+
         if (null != excluded) {
           for (String ex: excluded) {
             msg.addToExcluded(ex);
           }
         }
-        
+
         bytes = DatalogHelper.serialize(msg);
         // We do not encrypt the init message since the feeder does not yet know who we
         // are
@@ -309,7 +309,7 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
         //
         // Now emit a seek message
         //
-        
+
         msg.clear();
         if (this.offsetFile.exists()) {
           String seek = FileUtils.readFileToString(offsetFile, StandardCharsets.UTF_8).replaceAll("\n.*","").trim();
@@ -327,7 +327,7 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
           msg.setType(DatalogMessageType.TSEEK);
           msg.setSeekts(System.currentTimeMillis());
         }
-        
+
         System.out.println("SEEK MSG=" + msg);
         bytes = DatalogHelper.serialize(msg);
         if (encrypt) {
@@ -336,41 +336,41 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
         DatalogHelper.writeLong(out, bytes.length, 4);
         out.write(bytes);
         out.flush();
-        
+
         //
         // Now retrieve the DATA messages and push them to a worker
         //
-        
+
         DatalogRecord record = new DatalogRecord();
-        
+
         Map<String,String> labels = new HashMap<String,String>();
         labels.put(SENSISION_LABEL_CONSUMER, this.id);
         labels.put(SENSISION_LABEL_FEEDER, this.feeder);
-             
+
         while(true) {
-          
+
           //
           // Check if we should emit a commit or seek message (in case of failure)
           //
-          
+
           synchronized (failed) {
             // All inflight messages have been acknowledged
-            
+
             if (!inflight.isEmpty() && inflight.size() == failed.size() + successful.size()) {
               //
               // Scan the inflight refs and commit up before the first failed ref
               //
-              
+
               int idx = 0;
-              
+
               while (idx < inflight.size()) {
                 if (failed.contains(inflight.get(idx))) {
                   idx--;
-                  break;                  
+                  break;
                 }
                 idx++;
               }
-              
+
               // idx is < 0, this means the first commit ref failed
               if (idx < 0) {
                 //System.out.println(">>>SEEK " + inflight.get(0));
@@ -387,12 +387,12 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
                 out.flush();
                 inflight.clear();
                 failed.clear();
-                successful.clear();                
+                successful.clear();
               } else if (idx >= inflight.size()) {
                 //System.out.println(">>>COMMIT " + inflight.get(inflight.size() - 1));
                 // idx > inflight.size(), so all refs were successfully handled
                 // Send COMMIT message for last ref in infligt
-                
+
                 msg.clear();
                 msg.setType(DatalogMessageType.COMMIT);
                 msg.setRef(inflight.get(inflight.size() - 1));
@@ -407,7 +407,7 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
                 inflight.clear();
                 failed.clear();
                 successful.clear();
-                
+
               } else {
                 //System.out.println(">>>COMMIT " + inflight.get(idx));
                 //System.out.println(">>>SEEK " + inflight.get(idx + 1));
@@ -442,20 +442,20 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
               }
             }
           }
-          
+
           //
           // If less than 4 bytes are available, skip reading
           //
-          
+
           if (in.available() < 4) {
             LockSupport.parkNanos(10000000L);
             continue;
           }
-          
+
           //
           // Read the next message on the wire
           //
-          
+
           bytes = DatalogHelper.readBlob(in, 0);
           if (encrypt) {
             bytes = CryptoHelper.unwrapBlob(AES_KEY, bytes);
@@ -463,22 +463,22 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
 
           msg.clear();
           DatalogHelper.deserialize(bytes, msg);
-          
+
           if (DatalogMessageType.DATA != msg.getType()) {
             throw new IOException("Invalid message type " + msg.getType() + ", expected " + DatalogMessageType.DATA.name());
           }
-                    
+
           //
           // Extract the DatalogRecord
           //
-        
+
           record.clear();
           DatalogHelper.deserialize(msg.getRecord(), record);
-          
+
           //
-          // If our id created this message, ignore it, it would create a loop
+          // If our id or an exluded one created this message, ignore it, it would create a loop
           //
-          
+
           if (this.excluded.contains(record.getId())) {
             //System.out.println("SKIPPING " + msg.getCommitref());
             Sensision.update(SENSISION_CLASS_DATALOG_IGNORED, labels, 1);
@@ -490,83 +490,87 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
           //
           // Recompute the class and labels id with our own keys
           //
-          
+
           long classid = GTSHelper.classId(CLASS_KEYS, record.getMetadata().getName());
           long labelsid = GTSHelper.labelsId(LABELS_KEYS, record.getMetadata().getLabels());
-          
+
           record.getMetadata().setClassId(classid);
           record.getMetadata().setLabelsId(labelsid);
-          
+
           //
           // Check shards if needed
           //
-          
+
           if (hasShards) {
             // compute shardid
             long shifted = (labelsid >>> shardShift) & 0xFFFFFFFFL;
             shifted |= (classid << (64 - shardShift)) & 0xFFFFFFFFL;
             int sid = (int) shifted;
-            
+
             boolean matched = false;
-            
+
             for (int i = 0; i < modulus.length; i++) {
               if (0 != modulus[i] && remainder[i] == sid % modulus[i]) {
                 matched = true;
                 break;
               }
             }
-            
+
             if (!matched) {
               continue;
             }
           }
-          
+
+          //
+          // Execute the sharding macro if set
+          //
+
           //
           // Offer the message
           //
-          
+
           DatalogWorkers.offer(this, msg.getCommitref(), record);
           inflight.add(msg.getCommitref());
         }
-      } catch (Exception e) {        
+      } catch (Exception e) {
         LOG.error("", e);
       } finally {
         if (null != socket) {
           try {
             socket.close();
-          } catch (IOException ioe) {            
+          } catch (IOException ioe) {
           }
         }
-      }      
+      }
     }
   }
-  
+
   @Override
   public void init(KeyStore ks, String name) {
     this.feeder = name;
     this.suffix = "." + name;
-    
+
     this.CLASS_KEYS = SipHashInline.getKey(ks.getKey(KeyStore.SIPHASH_CLASS));
     this.LABELS_KEYS = SipHashInline.getKey(ks.getKey(KeyStore.SIPHASH_LABELS));
-    
+
     this.stack = new MemoryWarpScriptStack(null, null);
-    
+
     //
     // Extract ECC private/public key
     //
-        
+
     String eccpri = WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_ECC_PRIVATE + suffix);
     String eccpub = WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_ECC_PUBLIC + suffix);
 
     if (null == eccpri || null == eccpub) {
       throw new RuntimeException("Missing ECC keys.");
     }
-    
+
     String[] tokens = eccpri.split(":");
     Map<String,String> map = new HashMap<String,String>();
     map.put(Constants.KEY_CURVE, tokens[0]);
     map.put(Constants.KEY_D, tokens[1]);
-            
+
     try {
       stack.push(map);
       new ECPRIVATE(WarpScriptLib.ECPRIVATE).apply(stack);
@@ -579,58 +583,58 @@ public class TCPDatalogConsumer extends Thread implements DatalogConsumer {
     map.clear();
     map.put(Constants.KEY_CURVE, tokens[0]);
     map.put(Constants.KEY_Q, tokens[1]);
-      
+
     try {
       stack.push(map);
       new ECPUBLIC(WarpScriptLib.ECPUBLIC).apply(stack);
       eccPublic = (ECPublicKey) stack.pop();
     } catch (WarpScriptException wse) {
       throw new RuntimeException("Error extracting ECC public key.", wse);
-    }        
-     
+    }
+
     this.id = WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_ID + suffix);
-    
+
     if (null == this.id) {
       throw new RuntimeException("Missing Datalog Consumer id '" + FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_ID + suffix + "'.");
     }
-    
+
     this.excluded = new HashSet<String>();
-    
+
     if (null == WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_EXCLUDED + suffix)) {
       throw new RuntimeException("Excluded ids MUST be specified using '" + FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_EXCLUDED + suffix + "'.");
     }
-    
+
     String[] ids = WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_EXCLUDED + suffix).split(",");
     for (String id: ids) {
       excluded.add(id.trim());
     }
-    
+
     if (null == WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_OFFSETFILE + suffix)) {
       throw new RuntimeException("Missing offset file '" + FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_OFFSETFILE + suffix + "'.");
     }
-    
+
     this.offsetFile = new File(WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_OFFSETFILE + suffix));
-    
+
     this.offsetDelay = Long.parseLong(WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_CONSUMER_OFFSETDELAY + suffix, "0"));
-    
+
     this.setName("[Datalog Consumer " + name + "]");
     this.setDaemon(true);
     this.start();
   }
-  
+
   @Override
   public void failure(String ref) {
     synchronized (failed) {
       failed.add(ref);
     }
   }
-  @Override  
+  @Override
   public void success(String ref) {
     synchronized (failed) {
       successful.add(ref);
     }
   }
-  
+
   private void syncCommit(String ref) throws IOException {
     long now = System.currentTimeMillis();
     if (now - lastsync >= offsetDelay) {
