720752181faf9b53c8a230a4d83a054fcf404dd6
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2021-01-25 16:00:28 +0100

    Corrected exception messages

--- warp10/src/main/java/io/warp10/standalone/datalog/TCPDatalogFeeder.java
+++ warp10/src/main/java/io/warp10/standalone/datalog/TCPDatalogFeeder.java
@@ -28,53 +28,53 @@ import org.slf4j.LoggerFactory;
 import io.warp10.WarpConfig;
 
 public class TCPDatalogFeeder extends Thread {
-  
+
   private static final Logger LOG = LoggerFactory.getLogger(TCPDatalogFeeder.class);
-  
+
   private final FileBasedDatalogManager manager;
   private final int port = 4321;
   private final InetAddress addr;
   private final int backlog = 2;
-  
+
   private static final int MAX_CLIENTS = 2;
-  
+
   private final String checkmacro;
-  
+
   public TCPDatalogFeeder(FileBasedDatalogManager manager) {
     this.manager = manager;
-  
+
     this.checkmacro = WarpConfig.getProperty(FileBasedDatalogManager.CONFIG_DATALOG_FEEDER_CHECKMACRO);
-    
+
     if (null == checkmacro) {
       throw new RuntimeException("Missing '" + FileBasedDatalogManager.CONFIG_DATALOG_FEEDER_CHECKMACRO + "' configuration.");
     }
-    
+
     try {
       this.addr = InetAddress.getByName("127.0.0.1");
     } catch (Exception e) {
-      throw new RuntimeException("Error initializing Datalog TCP Forwarder.", e);
+      throw new RuntimeException("Error initializing Datalog TCP Feeder.", e);
     }
     this.setDaemon(true);
     this.setName("[Datalog TCP Feeder on port " + port + "]");
-    this.start();    
+    this.start();
   }
-  
+
   @Override
   public void run() {
     //
     // Listen on configured port
     //
-      
+
    ServerSocket server;
-   
+
    try {
     server = new ServerSocket(this.port, this.backlog, this.addr);
    } catch (Exception e) {
-     throw new RuntimeException("Unable to create server socket for Datalog TCP Forwarder host " + this.addr + " port " + this.port);
+     throw new RuntimeException("Unable to create server socket for Datalog TCP Feeder host " + this.addr + " port " + this.port);
    }
-   
+
    AtomicInteger clients = new AtomicInteger(0);
-   
+
    while(true) {
      try {
        Socket socket = server.accept();
@@ -82,15 +82,15 @@ public class TCPDatalogFeeder extends Thread {
          clients.decrementAndGet();
          socket.close();
        }
-       
+
        //
        // Allocate a new worker
        //
-       
+
        TCPDatalogFeederWorker worker = new TCPDatalogFeederWorker(this.manager, socket, clients, this.checkmacro);
      } catch (IOException ioe) {
        LOG.error("Error accepting incoming connection", ioe);
-     }     
+     }
    }
   }
 }
