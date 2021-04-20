07745e28392f64097b240331a3734d3c6a40be82
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2021-04-20 19:02:34 +0200

    Switched to using toString()

--- warp10/src/main/java/io/warp10/json/NamedWarpScriptFunctionSerializer.java
+++ warp10/src/main/java/io/warp10/json/NamedWarpScriptFunctionSerializer.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2020  SenX S.A.S.
+//   Copyright 2020-2021  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -16,16 +16,13 @@
 
 package io.warp10.json;
 
+import java.io.IOException;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
-import io.warp10.WarpURLEncoder;
 import io.warp10.script.NamedWarpScriptFunction;
-import io.warp10.script.WarpScriptLib;
-import io.warp10.script.WarpScriptStack;
-
-import java.io.IOException;
 
 public class NamedWarpScriptFunctionSerializer extends StdSerializer<NamedWarpScriptFunction> {
 
@@ -35,6 +32,6 @@ public class NamedWarpScriptFunctionSerializer extends StdSerializer<NamedWarpSc
 
   @Override
   public void serialize(NamedWarpScriptFunction namedWarpScriptFunction, JsonGenerator gen, SerializerProvider provider) throws IOException {
-    gen.writeString(namedWarpScriptFunction.refSnapshot());
+    gen.writeString(namedWarpScriptFunction.toString());
   }
 }
