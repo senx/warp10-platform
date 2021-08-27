813a4c8621dbd32d3a1b17aad510fac9d5c42c62
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2021-08-27 15:03:20 +0200

    Fixed copyright notice and typo

--- warp10/src/main/java/io/warp10/script/functions/RUNNERNONCE.java
+++ warp10/src/main/java/io/warp10/script/functions/RUNNERNONCE.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2018  SenX S.A.S.
+//   Copyright 2018-2021  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -50,7 +50,7 @@ public class RUNNERNONCE extends NamedWarpScriptFunction implements WarpScriptSt
     }
 
 
-    stack.push((getNonce((String) o));
+    stack.push(getNonce((String) o));
 
     return stack;
   }
