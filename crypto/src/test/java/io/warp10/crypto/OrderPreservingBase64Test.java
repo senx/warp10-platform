1251b37044917f39903de18729854339182f0611
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2022-06-09 15:27:56 +0200

    Added tests for buffered encodeTo{Stream.Writer}

--- crypto/src/test/java/io/warp10/crypto/OrderPreservingBase64Test.java
+++ crypto/src/test/java/io/warp10/crypto/OrderPreservingBase64Test.java
@@ -143,6 +143,94 @@ public class OrderPreservingBase64Test {
   }
 
   @Test
+  public void testEncodeToWriterBuffered() throws IOException {
+
+    StringWriter sw = new StringWriter();
+    byte[] buf = new byte[1024];
+
+    byte[] data = new byte[] { -1 };
+    OrderPreservingBase64.encodeToWriter(sw, data, 0, data.length, buf);
+    String str = sw.toString();
+
+    Assert.assertEquals(2, str.length());
+    Assert.assertEquals("zk", str);
+
+    data = new byte[] { -1, -1 };
+    sw = new StringWriter();
+    OrderPreservingBase64.encodeToWriter(sw, data, 0, data.length, buf);
+    str = sw.toString();
+
+    Assert.assertEquals(3, str.length());
+    Assert.assertEquals("zzw", str);
+
+    data = new byte[] { -1, -1, -1 };
+    sw = new StringWriter();
+    OrderPreservingBase64.encodeToWriter(sw, data, 0, data.length, buf);
+    str = sw.toString();
+
+    Assert.assertEquals(4, str.length());
+    Assert.assertEquals("zzzz", str);
+
+    data = new byte[] { -1, -1, -1, -1 };
+    sw = new StringWriter();
+    OrderPreservingBase64.encodeToWriter(sw, data, 0, data.length, buf);
+    str = sw.toString();
+
+    Assert.assertEquals(6, str.length());
+    Assert.assertEquals("zzzzzk", str);
+
+    data = new byte[] { -1, -1, -1, -1, -1 };
+    sw = new StringWriter();
+    OrderPreservingBase64.encodeToWriter(sw, data, 0, data.length, buf);
+    str = sw.toString();
+
+    Assert.assertEquals(7, str.length());
+    Assert.assertEquals("zzzzzzw", str);
+  }
+
+  @Test
+  public void testEncodeToStreamBuffered() throws IOException {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    byte[] buf = new byte[1024];
+
+    byte[] data = new byte[] { -1 };
+    OrderPreservingBase64.encodeToStream(out, data, 0, data.length, buf);
+    byte[] encoded = out.toByteArray();
+
+    Assert.assertEquals(2, encoded.length);
+    Assert.assertEquals("zk", new String(encoded));
+
+    data = new byte[] { -1, -1 };
+    out.reset();
+    OrderPreservingBase64.encodeToStream(out, data, 0, data.length, buf);
+    encoded = out.toByteArray();
+    Assert.assertEquals(3, encoded.length);
+    Assert.assertEquals("zzw", new String(encoded));
+
+    data = new byte[] { -1, -1, -1 };
+    out.reset();
+    OrderPreservingBase64.encodeToStream(out, data, 0, data.length, buf);
+    encoded = out.toByteArray();
+    Assert.assertEquals(4, encoded.length);
+    Assert.assertEquals("zzzz", new String(encoded));
+
+    data = new byte[] { -1, -1, -1, -1 };
+    out.reset();
+    OrderPreservingBase64.encodeToStream(out, data, 0, data.length, buf);
+    encoded = out.toByteArray();
+    Assert.assertEquals(6, encoded.length);
+    Assert.assertEquals("zzzzzk", new String(encoded));
+
+    data = new byte[] { -1, -1, -1, -1, -1 };
+    out.reset();
+    OrderPreservingBase64.encodeToStream(out, data, 0, data.length, buf);
+    encoded = out.toByteArray();
+    Assert.assertEquals(7, encoded.length);
+    Assert.assertEquals("zzzzzzw", new String(encoded));
+  }
+
+
+  @Test
   public void testDecode() {
     byte[] data = "zk".getBytes();
     byte[] decoded = OrderPreservingBase64.decode(data);
