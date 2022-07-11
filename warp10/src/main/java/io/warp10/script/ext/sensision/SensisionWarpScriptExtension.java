3e3ec73f00f6ddb460e3f75db7ba5435991dfcaa
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2022-07-11 16:29:19 +0200

    Added support for capabilities

--- warp10/src/main/java/io/warp10/script/ext/sensision/SensisionWarpScriptExtension.java
+++ warp10/src/main/java/io/warp10/script/ext/sensision/SensisionWarpScriptExtension.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2018  SenX S.A.S.
+//   Copyright 2018-2022  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package io.warp10.script.ext.sensision;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.warp10.WarpConfig;
 import io.warp10.warp.sdk.WarpScriptExtension;
 
 /**
@@ -26,22 +27,33 @@ import io.warp10.warp.sdk.WarpScriptExtension;
  * set/update/clear/get Sensision metrics
  */
 public class SensisionWarpScriptExtension extends WarpScriptExtension {
-  
+
   private static final Map<String, Object> functions;
-  
+
+  public static final String READ_CAPABILITY = "sensision.read";
+  public static final String WRITE_CAPABILITY = "sensision.write";
+  private static final String CONF_USE_CAPABILITY = "sensision.capability";
+  private static final boolean useCapability;
+
   static {
     functions = new HashMap<String, Object>();
-    
+
     functions.put("SENSISION.EVENT", new SENSISIONEVENT("SENSISION.EVENT"));
     functions.put("SENSISION.UPDATE", new SENSISIONUPDATE("SENSISION.UPDATE"));
     functions.put("SENSISION.SET", new SENSISIONSET("SENSISION.SET"));
     functions.put("SENSISION.GET", new SENSISIONGET("SENSISION.GET"));
     functions.put("SENSISION.DUMP", new SENSISIONDUMP("SENSISION.DUMP"));
     functions.put("SENSISION.DUMPEVENTS", new SENSISIONDUMPEVENTS("SENSISION.DUMPEVENTS"));
+
+    useCapability = "true".equals(WarpConfig.getProperty(CONF_USE_CAPABILITY, "false"));
   }
-  
+
   @Override
   public Map<String, Object> getFunctions() {
     return functions;
+  }
+
+  public static boolean useCapability() {
+    return useCapability;
   }
 }
