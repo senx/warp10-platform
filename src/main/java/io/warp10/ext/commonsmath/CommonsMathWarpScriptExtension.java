a5ab6de4b924bae7b45c5310e682f983dd0b182e
Jean-Charles Vialatte <jean-charles.vialatte@senx.io>
2022-07-21 12:42:59 +0200

    refac

--- src/main/java/io/warp10/ext/commonsmath/CommonsMathWarpScriptExtension.java
+++ src/main/java/io/warp10/ext/commonsmath/CommonsMathWarpScriptExtension.java
@@ -5,14 +5,14 @@ import java.util.Map;
 
 import io.warp10.warp.sdk.WarpScriptExtension;
 
-public class PleaseRenameWarpScriptExtension extends WarpScriptExtension {
+public class CommonsMathWarpScriptExtension extends WarpScriptExtension {
   
   private static final Map<String,Object> functions;
   
   static {
     functions = new HashMap<String,Object>();
     
-    functions.put("RENAME ME", new MICROSPHEREFIT("RENAME ME"));
+    functions.put("MICROSPHEREFIT", new MICROSPHEREFIT("MICROSPHEREFIT"));
   }
   
   @Override
