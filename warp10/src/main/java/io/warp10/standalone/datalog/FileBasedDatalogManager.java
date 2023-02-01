6d49e52a2b5c1d1410d3309966ae253cbe1eed87
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2023-02-01 10:21:18 +0100

    Fixed datalog file size computation

--- warp10/src/main/java/io/warp10/standalone/datalog/FileBasedDatalogManager.java
+++ warp10/src/main/java/io/warp10/standalone/datalog/FileBasedDatalogManager.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2020-2022  SenX S.A.S.
+//   Copyright 2020-2023  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -544,7 +544,7 @@ public class FileBasedDatalogManager extends DatalogManager implements Runnable 
         if (syncAll) {
           datalog.hsync();
         }
-        size.addAndGet(value.length);
+        size.set(datalog.getLength());
       } else {
         // FIXME(hbs): seems weird
         if (!syncAll) {
