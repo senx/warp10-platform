8ed31a97910e6dc201737c89a466f3bfcc6c7ada
Jean-Charles Vialatte <jean-charles.vialatte@senx.io>
2023-02-09 11:14:56 +0100

    check WarpscriptMapper type in MAP

--- warp10/src/main/java/io/warp10/script/functions/MAP.java
+++ warp10/src/main/java/io/warp10/script/functions/MAP.java
@@ -20,11 +20,12 @@ import io.warp10.continuum.gts.GTSHelper;
 import io.warp10.continuum.gts.GeoTimeSerie;
 import io.warp10.script.NamedWarpScriptFunction;
 import io.warp10.script.WarpScriptATCException;
+import io.warp10.script.WarpScriptException;
+import io.warp10.script.WarpScriptMapper;
 import io.warp10.script.WarpScriptMapperFunction;
+import io.warp10.script.WarpScriptStack;
 import io.warp10.script.WarpScriptStack.Macro;
 import io.warp10.script.WarpScriptStackFunction;
-import io.warp10.script.WarpScriptException;
-import io.warp10.script.WarpScriptStack;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -82,7 +83,7 @@ public class MAP extends NamedWarpScriptFunction implements WarpScriptStackFunct
       throw new WarpScriptException(getName() + " expects Geo Time Series or lists thereof as first parameters.");
     }
 
-    if (nseries == params.size() || !(params.get(nseries) instanceof WarpScriptMapperFunction) && !(params.get(nseries) instanceof Macro)) {
+    if (nseries == params.size() || !(params.get(nseries) instanceof WarpScriptMapper) && !(params.get(nseries) instanceof WarpScriptMapperFunction) && !(params.get(nseries) instanceof Macro)) {
       throw new WarpScriptException(getName() + " expects a mapper function or a macro after Geo Time Series.");
     }
 
