4924b7a051c618b327c7d04422a1eeb750339851
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2023-03-29 16:41:10 +0200

    Addressed PR comment

--- warp10/src/main/java/io/warp10/script/StackUtils.java
+++ warp10/src/main/java/io/warp10/script/StackUtils.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2018-2022  SenX S.A.S.
+//   Copyright 2018-2023  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -15,17 +15,6 @@
 //
 
 package io.warp10.script;
-
-//import io.warp10.json.JsonUtils;
-//import io.warp10.WarpURLEncoder;
-//import io.warp10.continuum.gts.GTSHelper;
-//import io.warp10.continuum.gts.GeoTimeSerie;
-//import io.warp10.continuum.gts.UnsafeString;
-//import io.warp10.continuum.gts.GeoTimeSerie.TYPE;
-//import io.warp10.continuum.store.thrift.data.Metadata;
-//import io.warp10.script.WarpScriptStack.Macro;
-//import io.warp10.script.functions.SNAPSHOT.Snapshotable;
-//import io.warp10.warp.sdk.WarpScriptJavaFunctionGTS;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -110,7 +99,7 @@ public class StackUtils {
         //
 
         line = UnsafeString.sanitizeStrings(line);
-        
+
         if (-1 != line.indexOf(' ')) {
           //statements = line.split(" +");
           statements = UnsafeString.split(line, ' ');
