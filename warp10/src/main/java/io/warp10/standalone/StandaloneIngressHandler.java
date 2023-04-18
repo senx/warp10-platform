2eef8597547e581e1f8c16a460603e09c3961c1b
pi-r-p <pierre.papin@senx.io>
2023-04-18 18:59:57 +0200

    log error when FDB client package is not installed

--- warp10/src/main/java/io/warp10/standalone/StandaloneIngressHandler.java
+++ warp10/src/main/java/io/warp10/standalone/StandaloneIngressHandler.java
@@ -34,7 +34,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import io.warp10.fdb.FDBStoreClient;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.joda.time.Instant;
