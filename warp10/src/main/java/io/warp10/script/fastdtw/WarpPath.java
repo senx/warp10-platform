0543469f4add852a54909c45330a55630c0d5458
Xavier Marin <xavier.marin@senx.io>
2023-04-21 11:38:01 +0200

    update copyrights

--- warp10/src/main/java/io/warp10/script/fastdtw/WarpPath.java
+++ warp10/src/main/java/io/warp10/script/fastdtw/WarpPath.java
@@ -1,5 +1,5 @@
 //
-//   Copyright 2018  SenX S.A.S.
+//   Copyright 2018-2023  SenX S.A.S.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
