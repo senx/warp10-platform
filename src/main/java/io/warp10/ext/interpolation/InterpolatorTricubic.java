61d420b49a0fc5a978cd69f2db6b15b65d61ef5a
Jean-Charles Vialatte <jean-charles.vialatte@senx.io>
2023-09-26 11:53:23 +0200

    tick instead of max long

--- src/main/java/io/warp10/ext/interpolation/InterpolatorTricubic.java
+++ src/main/java/io/warp10/ext/interpolation/InterpolatorTricubic.java
@@ -88,9 +88,9 @@ public class InterpolatorTricubic extends NamedWarpScriptFunction implements War
       if (3 != values.length) {
         throw new WarpScriptException(getName() + " expects 3 components but got " + values.length);
       }
-      
+
       if (null == values[0] || null == values[1] || null == values[2]) {
-        return new Object[] {Long.MAX_VALUE, GeoTimeSerie.NO_LOCATION, GeoTimeSerie.NO_ELEVATION, null};
+        return new Object[] {tick, GeoTimeSerie.NO_LOCATION, GeoTimeSerie.NO_ELEVATION, null};
       }
 
       double x = ((Number) values[0]).doubleValue();
