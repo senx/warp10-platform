42124025db1233ceb7b4068ccb613e3b36d5bdaa
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2023-10-10 11:59:35 +0200

    Initial commit of BigDecimal support

---
+++ warp10/src/main/java/io/warp10/script/functions/BDSCALE.java
@@ -0,0 +1,57 @@
+//
+//   Copyright 2023  SenX S.A.S.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+package io.warp10.script.functions;
+
+import java.math.BigDecimal;
+
+import io.warp10.script.NamedWarpScriptFunction;
+import io.warp10.script.WarpScriptException;
+import io.warp10.script.WarpScriptStack;
+import io.warp10.script.WarpScriptStackFunction;
+
+public class BDSCALE extends NamedWarpScriptFunction implements WarpScriptStackFunction {
+
+  public BDSCALE(String name) {
+    super(name);
+  }
+
+  @Override
+  public Object apply(WarpScriptStack stack) throws WarpScriptException {
+    Object o = stack.pop();
+
+    int scale = 1;
+
+    if (o instanceof BigDecimal) {
+      stack.push((long) ((BigDecimal) o).scale());
+      return stack;
+    }
+
+    if (o instanceof Long) {
+      scale = Math.toIntExact(((Long) o).longValue());
+    } else {
+      throw new WarpScriptException(getName() + " only supports LONG scale.");
+    }
+
+    o = stack.pop();
+
+    BigDecimal bd1 = TOBD.toBigDecimal(getName(), o);
+
+    stack.push(bd1.setScale(scale));
+
+    return stack;
+  }
+}
