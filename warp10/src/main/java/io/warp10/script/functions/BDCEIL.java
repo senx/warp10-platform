7a6df40a17d8ec3d5160c0356cf4bdbff437c95f
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2024-02-19 09:43:34 +0100

    Added BDROUND/CEIL/FLOOR

---
+++ warp10/src/main/java/io/warp10/script/functions/BDCEIL.java
@@ -0,0 +1,44 @@
+//
+//   Copyright 2024  SenX S.A.S.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+package io.warp10.script.functions;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+import io.warp10.script.NamedWarpScriptFunction;
+import io.warp10.script.WarpScriptException;
+import io.warp10.script.WarpScriptStack;
+import io.warp10.script.WarpScriptStackFunction;
+
+public class BDCEIL extends NamedWarpScriptFunction implements WarpScriptStackFunction {
+
+  public BDCEIL(String name) {
+    super(name);
+  }
+
+  @Override
+  public Object apply(WarpScriptStack stack) throws WarpScriptException {
+    Object o = stack.pop();
+
+    BigDecimal bd1 = TOBD.toBigDecimal(getName(), o);
+
+    stack.push(bd1.setScale(0, RoundingMode.CEILING));
+
+    return stack;
+  }
+}
---
+++ warp10/src/main/java/io/warp10/script/functions/BDROUND.java
@@ -0,0 +1,43 @@
+//
+//   Copyright 2024  SenX S.A.S.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+package io.warp10.script.functions;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import io.warp10.script.NamedWarpScriptFunction;
+import io.warp10.script.WarpScriptException;
+import io.warp10.script.WarpScriptStack;
+import io.warp10.script.WarpScriptStackFunction;
+
+public class BDROUND extends NamedWarpScriptFunction implements WarpScriptStackFunction {
+
+  public BDROUND(String name) {
+    super(name);
+  }
+
+  @Override
+  public Object apply(WarpScriptStack stack) throws WarpScriptException {
+    Object o = stack.pop();
+
+    BigDecimal bd1 = TOBD.toBigDecimal(getName(), o);
+
+    stack.push(bd1.setScale(0, RoundingMode.HALF_UP));
+
+    return stack;
+  }
+}
